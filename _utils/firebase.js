a2f117e1cf21146dbe6136dd0a81b77d73deb056
Pairphtrp <phattharaporniamcharoen@gmail.com>
2025-04-21 01:59:21 -0600

    firebase setting - based week9

---
+++ _utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNEtHbL1kIzoVKgWIWnR_GCpIfz3TKhRM",
+  authDomain: "todotas-2bb41.firebaseapp.com",
+  projectId: "todotas-2bb41",
+  storageBucket: "todotas-2bb41.firebasestorage.app",
+  messagingSenderId: "407850438583",
+  appId: "1:407850438583:web:af4b5089b10e58bb7aa232"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
